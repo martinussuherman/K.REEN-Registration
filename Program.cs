86b9b0accaf4a6ba167d7f0117221c8dc83a66e9
Martinus Suherman <martinus.suherman@gmail.com>
2022-12-01 17:07:44 +0700

    Update Program.cs - add OIDC auth configuration
    Ref: https://damienbod.com/2019/10/11/securing-an-asp-net-core-razor-page-app-using-openid-connect-code-flow-with-pkce/

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+ConfigureAuthentication(builder);
+
+builder.Services.AddAuthorization();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -18,8 +24,53 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
 
 app.Run();
+
+
+void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    IConfigurationSection bearerConfiguration = builder.Configuration.GetSection("Bearer");
+    string[]? scopes = bearerConfiguration
+        .GetSection("Scopes")?
+        .GetChildren()?
+        .Select(x => x.Value)?
+        .ToArray();
+
+    builder.Services
+        .AddAuthentication(options =>
+        {
+            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        })
+        .AddCookie()
+        .AddOpenIdConnect(options =>
+        {
+            options.SignInScheme = "Cookies";
+            options.Authority = bearerConfiguration.GetValue<string>("Authority");
+            options.RequireHttpsMetadata = true;
+            options.ClientId = bearerConfiguration.GetValue<string>("ClientId");
+            options.ClientSecret = bearerConfiguration.GetValue<string>("ClientSecret");
+            options.ResponseType = "code";
+            options.UsePkce = true;
+            options.SaveTokens = true;
+            AddScopes(options, scopes);
+        });
+}
+
+void AddScopes(OpenIdConnectOptions options, string[]? scopes)
+{
+    if (scopes == null)
+    {
+        return;
+    }
+
+    for (int counter = 0; counter < scopes.Length; counter++)
+    {
+        options.Scope.Add(scopes[counter]);
+    }
+}
