9ea007965a1a65409e99aa75203a2a9e0131751c
Martinus Suherman <martinus.suherman@gmail.com>
2022-12-05 11:10:56 +0700

    Add view model: ProvinsiView

---
+++ ViewModels/ProvinsiView.cs
@@ -0,0 +1,8 @@
+﻿namespace KReenRegistration.ViewModels
+{
+    public class ProvinsiView
+    {
+        public byte Kode { get; set; }
+        public string Nama { get; set; } = string.Empty;
+    }
+}
