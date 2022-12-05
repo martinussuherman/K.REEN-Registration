5e3c8679e36ea17517026cb30afdc2ac514f4f79
Martinus Suherman <martinus.suherman@gmail.com>
2022-12-05 10:01:40 +0700

    Update DataKontak.cs:
    - Fix for non-nullable property must contain a non-null value when exiting constructor warning

--- Models/DataKontak.cs
+++ Models/DataKontak.cs
@@ -6,15 +6,15 @@ namespace KReenRegistration.Models
     public partial class DataKontak
     {
         public uint Id { get; set; }
-        public string Nama { get; set; }
-        public string Email { get; set; }
-        public string NomorHp { get; set; }
+        public string Nama { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string NomorHp { get; set; } = string.Empty;
         public DateOnly TanggalLahir { get; set; }
-        public string JenisKelamin { get; set; }
-        public string Alamat { get; set; }
+        public string JenisKelamin { get; set; } = string.Empty;
+        public string Alamat { get; set; } = string.Empty;
         public ushort KodeKabupatenKota { get; set; }
-        public string KodePos { get; set; }
+        public string KodePos { get; set; } = string.Empty;
 
-        public virtual KabupatenKota KabupatenKotaNavigation { get; set; }
+        public virtual KabupatenKota? KabupatenKotaNavigation { get; set; }
     }
 }
