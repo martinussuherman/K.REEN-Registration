83a889cac467f038185a9428a87b1b47579c8091
Martinus Suherman <martinus.suherman@gmail.com>
2022-12-09 16:09:13 +0700

    Add view model: KabupatenKotaView

---
+++ ViewModels/KabupatenKotaView.cs
@@ -0,0 +1,9 @@
+namespace KReenRegistration.ViewModels
+{
+    public class KabupatenKotaView
+    {
+        public ushort Kode { get; set; }
+        public string Nama { get; set; } = string.Empty;
+        public byte KodeProvinsi { get; set; }
+    }
+}
