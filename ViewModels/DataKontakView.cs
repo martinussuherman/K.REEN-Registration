c6528876cb285466b4cf4be5c6bcfe6911dab2a7
Martinus Suherman <martinus.suherman@gmail.com>
2022-12-12 10:23:18 +0700

    Add view model: DataKontakView

---
+++ ViewModels/DataKontakView.cs
@@ -0,0 +1,15 @@
+﻿namespace KReenRegistration.ViewModels
+{
+    public class DataKontakView
+    {
+        public uint Id { get; set; }
+        public string Nama { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string NomorHp { get; set; } = string.Empty;
+        public DateOnly TanggalLahir { get; set; }
+        public string JenisKelamin { get; set; } = string.Empty;
+        public string Alamat { get; set; } = string.Empty;
+        public ushort KodeKabupatenKota { get; set; }
+        public string KodePos { get; set; } = string.Empty;
+    }
+}
