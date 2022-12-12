a7981b7c5afdc18dafd00e301bd70ae7a9b88dd6
Martinus Suherman <martinus.suherman@gmail.com>
2022-12-12 10:38:22 +0700

    Add ListController.cs - API controller for item list

---
+++ Controllers/ListController.cs
@@ -0,0 +1,43 @@
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using KReenRegistration.Models;
+using KReenRegistration.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KReenRegistration.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ListController : ControllerBase
+    {
+        public ListController(KreenContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet(nameof(Provinsi))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<List<ProvinsiView>> Provinsi()
+        {
+            return await _context.Provinsi
+               .AsNoTracking()
+               .ProjectTo<ProvinsiView>(_mapper.ConfigurationProvider)
+               .ToListAsync();
+        }
+
+        [HttpGet(nameof(KabupatenKota))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<List<KabupatenKotaView>> KabupatenKota()
+        {
+            return await _context.KabupatenKota
+               .AsNoTracking()
+               .ProjectTo<KabupatenKotaView>(_mapper.ConfigurationProvider)
+               .ToListAsync();
+        }
+
+        private readonly KreenContext _context;
+        private readonly IMapper _mapper;
+    }
+}
