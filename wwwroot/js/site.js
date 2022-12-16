f83b6205a4383561fdde9447f5f4a19497b663e6
Martinus Suherman <martinus.suherman@gmail.com>
2022-12-16 10:47:35 +0700

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,15 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function addFormSubmitListener(form, hook) {
+    form.addEventListener("submit", function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        if (hook != null) {
+            hook();
+        }
+        form.classList.add("was-validated");
+    }, false);
+}
