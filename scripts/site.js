e57de8c825e458272a58bb31190c46b332201722
Martinus Suherman <martinus.suherman@gmail.com>
2022-12-16 11:01:01 +0700

    Update script - add function flatpickrValidateRequired

--- scripts/site.js
+++ scripts/site.js
@@ -1,4 +1,3 @@
-"use strict";
 (function () {
     "use strict";
     let registerForm = document.getElementById("registerForm");
@@ -16,3 +15,20 @@ function addFormSubmitListener(form, hook) {
         form.classList.add("was-validated");
     }, false);
 }
+function flatpickrValidateRequired(dates, currentDateString, self, data) {
+    if (!self.input.required) {
+        return;
+    }
+    if (dates.length == 0) {
+        self.input.classList.add("is-invalid");
+        self.input.classList.remove("is-valid");
+        self.altInput?.classList.add("is-invalid");
+        self.altInput?.classList.remove("is-valid");
+        return;
+    }
+    self.input.classList.add("is-valid");
+    self.input.classList.remove("is-invalid");
+    self.altInput?.classList.add("is-valid");
+    self.altInput?.classList.remove("is-invalid");
+}
+export {};
--- scripts/site.ts
+++ scripts/site.ts
@@ -1,3 +1,5 @@
+import flatpickr from "flatpickr";
+
 (function () {
     "use strict";
 
@@ -18,4 +20,27 @@ function addFormSubmitListener(form: HTMLFormElement, hook?: () => void) {
 
         form.classList.add("was-validated");
     }, false);
+}
+
+function flatpickrValidateRequired(
+    dates: Date[],
+    currentDateString: string,
+    self: flatpickr.Instance,
+    data?: any) {
+    if (!self.input.required) {
+        return;
+    }
+
+    if (dates.length == 0) {
+        self.input.classList.add("is-invalid");
+        self.input.classList.remove("is-valid");
+        self.altInput?.classList.add("is-invalid");
+        self.altInput?.classList.remove("is-valid");
+        return;
+    }
+
+    self.input.classList.add("is-valid");
+    self.input.classList.remove("is-invalid");
+    self.altInput?.classList.add("is-valid");
+    self.altInput?.classList.remove("is-invalid");
 }
