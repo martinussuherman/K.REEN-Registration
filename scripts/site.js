56d87d797004b3b17dc64766639ad050adeacd65
Martinus Suherman <martinus.suherman@gmail.com>
2022-12-16 16:51:18 +0700

    Update script
    - Refactor function flatpickrValidateRequired: use self.input.validity.valid

--- scripts/site.js
+++ scripts/site.js
@@ -24,20 +24,17 @@ function addFormSubmitListener(form, hook) {
     }, false);
 }
 function flatpickrValidateRequired(dates, currentDateString, self, data) {
-    if (!self.input.required) {
+    if (self.input.validity.valid) {
+        self.input.classList.add("is-valid");
+        self.input.classList.remove("is-invalid");
+        self.altInput?.classList.add("is-valid");
+        self.altInput?.classList.remove("is-invalid");
         return;
     }
-    if (dates.length == 0) {
-        self.input.classList.add("is-invalid");
-        self.input.classList.remove("is-valid");
-        self.altInput?.classList.add("is-invalid");
-        self.altInput?.classList.remove("is-valid");
-        return;
-    }
-    self.input.classList.add("is-valid");
-    self.input.classList.remove("is-invalid");
-    self.altInput?.classList.add("is-valid");
-    self.altInput?.classList.remove("is-invalid");
+    self.input.classList.add("is-invalid");
+    self.input.classList.remove("is-valid");
+    self.altInput?.classList.add("is-invalid");
+    self.altInput?.classList.remove("is-valid");
 }
 function triggerInputChangeEvent() {
     let picker = document.getElementById("tanggalLahir")._flatpickr;
--- scripts/site.ts
+++ scripts/site.ts
@@ -38,22 +38,18 @@ function flatpickrValidateRequired(
     currentDateString: string,
     self: flatpickr.Instance,
     data?: any) {
-    if (!self.input.required) {
+    if (self.input.validity.valid) {
+        self.input.classList.add("is-valid");
+        self.input.classList.remove("is-invalid");
+        self.altInput?.classList.add("is-valid");
+        self.altInput?.classList.remove("is-invalid");
         return;
     }
 
-    if (dates.length == 0) {
-        self.input.classList.add("is-invalid");
-        self.input.classList.remove("is-valid");
-        self.altInput?.classList.add("is-invalid");
-        self.altInput?.classList.remove("is-valid");
-        return;
-    }
-
-    self.input.classList.add("is-valid");
-    self.input.classList.remove("is-invalid");
-    self.altInput?.classList.add("is-valid");
-    self.altInput?.classList.remove("is-invalid");
+    self.input.classList.add("is-invalid");
+    self.input.classList.remove("is-valid");
+    self.altInput?.classList.add("is-invalid");
+    self.altInput?.classList.remove("is-valid");
 }
 
 function triggerInputChangeEvent() {
