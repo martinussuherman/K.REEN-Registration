fb91d3b443b6419f1343121aeef348a1a6f9d92a
Martinus Suherman <martinus.suherman@gmail.com>
2023-05-10 15:12:07 +0700

    Update DataAffiliate.cs:
    - Fix for non-nullable property must contain a non-null value when exiting constructor warning

--- Models/DataAffiliate.cs
+++ Models/DataAffiliate.cs
@@ -7,14 +7,14 @@ namespace KReenRegistration.Models
     {
         public uint Id { get; set; }
         public short WpamId { get; set; }
-        public string FriendlyId { get; set; }
-        public string Nama { get; set; }
-        public string Email { get; set; }
-        public string NomorHp { get; set; }
-        public string Instagram { get; set; }
-        public string Tiktok { get; set; }
-        public string Facebook { get; set; }
-        public string KodeReferral { get; set; }
+        public string FriendlyId { get; set; } = string.Empty;
+        public string Nama { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string NomorHp { get; set; } = string.Empty;
+        public string Instagram { get; set; } = string.Empty;
+        public string Tiktok { get; set; } = string.Empty;
+        public string Facebook { get; set; } = string.Empty;
+        public string KodeReferral { get; set; } = string.Empty;
         public byte Approved { get; set; }
     }
 }
