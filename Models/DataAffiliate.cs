cf2341de7b05fb162507c865161ba81b02a87494
Martinus Suherman <martinus.suherman@gmail.com>
2023-05-10 15:01:08 +0700

    Update DataAffiliate.cs:
    - Update model from scaffolding

--- Models/DataAffiliate.cs
+++ Models/DataAffiliate.cs
@@ -6,7 +6,14 @@ namespace KReenRegistration.Models
     public partial class DataAffiliate
     {
         public uint Id { get; set; }
+        public short WpamId { get; set; }
+        public string FriendlyId { get; set; }
         public string Nama { get; set; }
+        public string Email { get; set; }
+        public string NomorHp { get; set; }
+        public string Instagram { get; set; }
+        public string Tiktok { get; set; }
+        public string Facebook { get; set; }
         public string KodeReferral { get; set; }
         public byte Approved { get; set; }
     }
